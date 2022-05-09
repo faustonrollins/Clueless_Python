b9771cc539325521312dccb095af11f59c300d80
JRScelza <scelzajr@gmail.com>
2022-05-09 12:57:43 -0400

    add other switch case for rooms

--- Server.py
+++ Server.py
@@ -227,7 +227,7 @@ def player_turn(nickname):
     player_id.send(f"You have rolled: {dice_count}.".encode("utf-8"))
     send_all(f"{nickname} rolled: {dice_count}", ex_id=player_id)
     player_point[nickname] += dice_count
-    if player_point[nickname] >= 8:
+    if player_point[nickname] > 7 and player_point[nickname] < 9:
         player_id.send("\nWant to enter in a room ? (y/n)".encode("utf-8"))
         choice = player_id.recv(1024).decode("utf-8")
         if choice[-1] == 'y':
@@ -294,6 +294,66 @@ def player_turn(nickname):
                 pass
         else:
             pass
+
+    elif player_point[nickname] > 8:
+        room_no = random.randint(1,6)
+        diagonal_room = rooms[room_no]
+        player_id.send(("\nEnter room: %s?" % diagonal_room).encode("utf-8"))
+        room_choice = player_id.recv(1024).decode("utf-8")
+        if room_choice[-1] == 'y':
+            player_point[nickname] = 0
+            player_id.send("\nChoose Suspect and Weapon. (separated by space)".encode("utf-8"))
+            time.sleep(0.5)
+            player_id.send(option_table.encode("utf-8"))
+            sus_wea = [0, 0]
+            while sus_wea[0] > 6 or sus_wea[0] < 1 or type(sus_wea[0]) != int or len(sus_wea) != 2:
+                # ..................................................................To check if entered option is valid.
+                try:
+                    sus_wea = list(map(int, player_id.recv(1024).decode("utf-8").split(": ")[1].split(" ")))
+                except Exception as er:
+                    print(f"Invalid Character Entered: {er}")
+                    player_id.send("Invalid Character selected!".encode("utf-8"))
+                    sus_wea = [0, 0]
+            while sus_wea[1] > 6 or sus_wea[1] < 1 or type(sus_wea[1]) != int or len(sus_wea) != 2:
+                # ..................................................................To check if entered option is valid.
+                try:
+                    sus_wea = list(map(int, player_id.recv(1024).decode("utf-8").split(": ")[1].split(" ")))
+
+                except Exception as er:
+                    print(f"Invalid Weapon Entered: {er}")
+                    player_id.send("Invalid Character selected!".encode("utf-8"))
+                    sus_wea = [0, 0]
+            send_all(f"\n{nickname}'s suggestion:")
+            send_all(suggestion.format((suspects[sus_wea[0]]), weapon[sus_wea[1]], rooms[room_no]))
+            accused = [suspects[sus_wea[0]], weapon[sus_wea[1]], rooms[room_no]]
+            time.sleep(2)
+            for name in nicknames:
+                for accuse in accused:
+                    if accuse in players_deck[name] and name != nickname:
+                        send_all(f"{name} has disapproved {nickname}'s suggestion.", player_id)
+                        player_id.send(f"{name} has {accuse}.".encode("utf-8"))
+                        temp_win = False
+                        break
+                if not temp_win:
+                    break
+            if temp_win:
+                send_all(f"No proof against {nickname}'s suggestion.")
+            player_id.send("Do you want to revel cards ?(y/n)".encode("utf-8"))
+            choice_r = player_id.recv(1024).decode("utf-8")[-1]
+            if choice_r == 'y':
+                if secret_deck["Killer"] == suspects[sus_wea[0]] and secret_deck["Weapon"] == weapon[sus_wea[1]] and \
+                        secret_deck["Place"] == rooms[room_no]:
+                    send_all(f"{nickname} WON !")
+                    player_id.send(f"\nCongrats {nickname} you have solved the case !".encode("utf-8"))
+                    return True
+                else:
+                    send_all(f"Wrong accusation !\n{nickname} will no longer make accusations.")
+                    nicknames.remove(nickname)
+            else:
+                pass
+        else:
+            pass
+    
     return False
 
 
